e0f384c5be9129e27a6b89a06714ce3fd3182af2
Tomás Atanasoff <49059091@ort>
2024-07-31 15:16:20 -0300

    Parse toml

--- Sin_interfaz/config.py
+++ Sin_interfaz/config.py
@@ -4,10 +4,23 @@ config = {
     "name": None,
 }
 
+def parse_toml(file: str) -> dict:
+    toml = open(f"{file}.toml", "r")
+    config = {}
+    keyword: str;
+    value: any;
+    for line in toml:
+        line = line.split(" ")
+        keyword = line[0]
+        value = line[2]
+        config[keyword] = value
+    return config
+                
+
 def cargar_config(file="chat") -> dict:
     '''Carga la configuración, si no la encuentra utiliza la ip como nombre'''
     try:
-        config_file = open(f"{file}.toml", "r")
+        return parse_toml(f"{file}.toml")
     except FileNotFoundError:
         print(f"File {file}.toml not found, using defaults")
         config["name"] = obtener_ip_local()
