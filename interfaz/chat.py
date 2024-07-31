b149a74a9e2d109fa86f8a06949e9e54f76cc58f
GANDELMAN URIEL <96003020@ort>
2024-07-31 10:07:00 -0300

    bugFixes

--- interfaz/chat.py
+++ interfaz/chat.py
@@ -5,7 +5,6 @@ from tkinter import scrolledtext, simpledialog, messagebox
 
 PORT = 12345        
 
-
 def recibir_mensajes(sock, chat_box):
     while True:
         try:
@@ -59,7 +58,7 @@ ip_local = obtener_ip_local()
 # Configuración de la interfaz gráfica
 root = tk.Tk()
 root.title("Chat Grupal")
-root.geometry("400x400")
+root.geometry("400x485")
 
 # Mostrar la IP local
 ip_label = tk.Label(root, text=f"Tu IP: {ip_local}:{PORT}")
@@ -67,16 +66,20 @@ ip_label.pack(padx=10, pady=5)
 
 chat_box = scrolledtext.ScrolledText(root, state=tk.DISABLED, wrap=tk.WORD)
 chat_box.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
-    
+
 mensaje_frame = tk.Frame(root)
 mensaje_frame.pack(padx=10, pady=10, fill=tk.X)
 
 mensaje_entry = tk.Entry(mensaje_frame)
-mensaje_entry.pack(side=tk.LEFT, fill=tk.X, expand=True)
+mensaje_entry.pack(fill=tk.X)
+mensaje_entry.grid(row=0, column=0, sticky="ew")
 mensaje_entry.bind("<Return>", enviar)
 
 enviar_button = tk.Button(mensaje_frame, text="Enviar", command=enviar)
-enviar_button.pack(side=tk.RIGHT)
+enviar_button.grid(row=0, column=1)
+root.attributes('-topmost', True)
+
+mensaje_frame.columnconfigure(0, weight=1)
 
 destinos = []
 while True:
